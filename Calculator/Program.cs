12cf07bf9253f6309a3e0c69db3bee3bd666e698
Vibha Rana <ranavibha02@gmail.com>
2019-10-30 15:22:48 -0700

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring Variables for equation
+            double result = 0;
+            double firstNumber = SetNumber("Type you first number : ");
+            double secondNumber = SetNumber("Type you second number: ");
+
+            string stringOperation =
+                SetOperation(
+                    "Enter the operation + (addition), - (subtraction), * (multiplication), / (division), % (Remainder));
+    
+            //This is my main menu
+                Console.WriteLine("***********Calculator Menu ************");
+            Console.WriteLine("1: Addition (+)");
+            Console.WriteLine("1: Subtraction (-)");
+            Console.WriteLine("1: Multiplication (x)");
+            Console.WriteLine("1: Division (/)");
+            Console.WriteLine("1: Remainder (%)");
+            Console.WriteLine("1: Exit");
+
+            //if else statements 
+
+
+
+            var operationChoice = Console.ReadLine();
+            while(operationChoice != "6")
+            {
+
+                decimal a = GetOperandA();
+                decimal b = GetOperandB();
+                Console.WriteLine(DoCalculation());
+
+            }
+        }
+    }
+}
